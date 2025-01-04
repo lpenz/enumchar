7535c98b36e66e378289b074beede7f960499765
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-01-04 20:00:19 +0000

    Simplify and increment tests

--- tests/test_enumchar.rs
+++ tests/test_enumchar.rs
@@ -4,9 +4,24 @@
 
 use enumchar::EnumChar;
 
-#[derive(EnumChar, Default, Debug, PartialEq, Eq)]
+#[derive(EnumChar, Debug, PartialEq, Eq)]
 pub enum ECAllVariants {
-    #[default]
+    #[char('#')]
+    Wall,
+    #[char('.')]
+    Empty,
+}
+
+#[test]
+pub fn test_allvariants_tryfrom_char() -> Result<(), String> {
+    assert_eq!(ECAllVariants::try_from('.')?, ECAllVariants::Empty);
+    assert_eq!(ECAllVariants::try_from('#')?, ECAllVariants::Wall);
+    assert!(matches!(ECAllVariants::try_from('z'), Err(_)));
+    Ok(())
+}
+
+#[derive(EnumChar, Debug, PartialEq, Eq)]
+pub enum ECSomeVariants {
     #[char('#')]
     Wall,
     #[char('.')]
@@ -15,9 +30,9 @@ pub enum ECAllVariants {
 }
 
 #[test]
-pub fn test_tryfrom_char() -> Result<(), String> {
-    assert_eq!(ECAllVariants::try_from('.')?, ECAllVariants::Empty);
-    assert_eq!(ECAllVariants::try_from('#')?, ECAllVariants::Wall);
-    assert!(matches!(ECAllVariants::try_from('z'), Err(_)));
+pub fn test_somevariants_tryfrom_char() -> Result<(), String> {
+    assert_eq!(ECSomeVariants::try_from('.')?, ECSomeVariants::Empty);
+    assert_eq!(ECSomeVariants::try_from('#')?, ECSomeVariants::Wall);
+    assert!(matches!(ECSomeVariants::try_from('z'), Err(_)));
     Ok(())
 }
